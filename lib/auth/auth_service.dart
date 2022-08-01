6eb1e68e9fbdbc386028dabc6d3564644829cf2a
rasel-hossen <straselhossen24@gmail.com>
2022-08-02 01:37:42 +0600

    update

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -5,14 +5,35 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   static final FirebaseAuth _auth = FirebaseAuth.instance;
   static final User? user = _auth.currentUser;
-  
-  static Future<bool> logIn (String email, String password) async {
-    final credensial = await _auth.signInWithEmailAndPassword(email: email, password: password);
+
+  static Future<bool> logIn(String email, String password) async {
+    final credensial = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
     return credensial.user != null;
   }
 
-  static Future<bool> register (String email, String password) async {
-    final credensial = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  static Future<bool> register(String email, String password) async {
+    final credensial = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
     return credensial.user != null;
+  }
+
+  static Future<void> logOut() => _auth.signOut();
+
+  static bool isEmailVerifyed() => _auth.currentUser!.emailVerified;
+
+  static Future<void> sendVeryficationMail() =>
+      _auth.currentUser!.sendEmailVerification();
+
+  static Future<void> updateDisplayNamer(String name) =>
+      _auth.currentUser!.updateDisplayName(name);
+
+  static Future<void> updatePhoneNumber(String phone) async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      verificationCompleted: (PhoneAuthCredential credential) {},
+      verificationFailed: (FirebaseAuthException e) {},
+      codeSent: (String verificationid, int? resendToken) {},
+      codeAutoRetrievalTimeout: (String verificationid) {},
+    );
   }
 }
